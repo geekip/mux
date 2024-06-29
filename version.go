ce38fb202ef1ff530de41ed812cadaee3a84d8fa
geekip <mail@yangfei.site>
2024-06-29 23:06:17 +0800

    New Version Description

---
+++ version.go
@@ -0,0 +1,8 @@
+// Copyright 2024 Geekip. All rights reserved.
+// Use of this source code is governed by a MIT style.
+// at https://github.com/geekip/mux
+
+package mux
+
+// Version is the current gin framework's version.
+const Version = "v1.0.0"
